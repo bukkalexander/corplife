315aa74ab2a1c7093ebf70136ba71d173e3d67ad
terrabukk <alexander.bukk@gmail.com>
2024-10-23 15:45:28 +0200

    modify app to dynamically add list items

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,23 +1,21 @@
 import React from 'react'
 
 function App() {
-
+  const question = {text:"What does AWS S3 do",answers:["S3 bla 1","S3 bla 2","S3 bla 3","S3 bla 4"]};
   return (
       <div id="questionApp">
         <div id="questionHeader">
-          <h1>Question Text</h1>
+          <h1>"Quiz"</h1>
         </div>
         <form id="questionForm">
           <h1>1/20</h1>
-          <p>What does AWS S3 do</p>
-          <input type="radio" name="q1" />
-          <label htmlFor="q1"> S3 bla 1</label><br />
-          <input type="radio" name="q2" />
-          <label htmlFor="q2"> S3 bla 2</label><br />
-          <input type="radio" name="q3" />
-          <label htmlFor="q3"> S3 bla 3</label><br />
-          <input type="radio" name="q4" />
-          <label htmlFor="q4"> S3 bla 4</label><br />
+          <p>{question.text}</p>
+          {question.answers.map((answer, index) => (
+            <>
+              <input type="radio" name={`q${index}`} />
+              <label htmlFor={`q${index}`}> {answer}</label><br />
+            </>
+          ))}
           <button type="submit">Submit</button>
         </form>
       </div>
